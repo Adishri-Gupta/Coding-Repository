a5abd747b5d8993969c4f6a63c5bcbed4401dd49
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-07-08 02:43:43 -0700

    Time: 64 ms (34.44%), Space: 14 MB (8.38%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,17 +1,19 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        length = 1
-        next_expected = None
-
-        if len(nums) < 2:
-            return length
-
-        for i in range(1, len(nums)):
-            if nums[i] - nums[i-1] > 0 and (next_expected == "UP" or next_expected == None):
-                next_expected = "DOWN"
-                length += 1
-            elif nums[i] - nums[i-1] < 0 and (next_expected == "DOWN" or next_expected ==None):
-                next_expected = "UP"
-                length += 1
-        return length
+        lenVal=1
+        flag=None
+        if len(nums)<2:
+            return lenVal
+        
+        for i in range(1,len(nums)):
+            if nums[i] - nums[i-1] > 0 and (flag == "UP" or flag == None):
+                flag = "DOWN"
+                lenVal += 1
+            elif nums[i] - nums[i-1] < 0 and (flag == "DOWN" or flag == None):
+                flag = "UP"
+                lenVal += 1
+        
+        
+        
+        return lenVal
     
