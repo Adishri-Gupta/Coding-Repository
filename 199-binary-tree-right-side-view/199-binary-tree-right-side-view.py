7a05c9e66b4335a660ef7f913e8669f7c64e1c34
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-07-11 01:31:22 -0700

    Time: 36 ms (88.32%), Space: 13.9 MB (22.93%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -9,31 +9,24 @@ class Solution:
         ans = []
         if root is None:
             return ans
-
-        # Initialize queue 
+ 
         queue = collections.deque()
         queue.append(root)
 
-        # Iterate over the queue until it's empty
         while queue:
-            # Check the length of queue
             currSize = len(queue)
             currList = []
 
             while currSize > 0:
-                # Dequeue element
+            
                 currNode = queue.popleft()
                 currList.append(currNode.val)
                 currSize -= 1
 
-                # Check for left child
                 if currNode.left is not None:
                     queue.append(currNode.left)
-                # Check for right child
                 if currNode.right is not None:
                     queue.append(currNode.right)
-
-            # Append the currList to answer after each iteration
             ans.append(currList[-1])
         return ans
         
