0493441b0579e804b3a24123b34c3108e024b170
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-07-13 12:56:25 -0700

    Time: 53 ms (99.41%), Space: 13.8 MB (99.43%) - LeetHub

---
+++ 256-paint-house/256-paint-house.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        n=len(costs)
+        red=costs[0][0]
+        blue=costs[0][1]
+        green=costs[0][2]
+        
+        for i in range(1,n):
+            red, blue, green=min(blue,green)+costs[i][0], min(red,green)+costs[i][1], min(red,blue)+costs[i][2]
+
+        
+        return min(red,blue,green)
