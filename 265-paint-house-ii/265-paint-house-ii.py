aed2d89dce64f6b3a9fd76431144c70992eed368
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-10 01:30:49 -0700

    Time: 166 ms (78.80%), Space: 14.1 MB (43.83%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.py
+++ 265-paint-house-ii/265-paint-house-ii.py
@@ -1,11 +1,10 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
-
         k=len(costs[0])
-       
+        n=len(costs)
+        mat = [costs[0][i] for i in range(k)]
+        for i in range(1,n):
             
-        for i in range(1,len(costs)):
             for j in range(k):
-                costs[i][j] = costs[i][j] + min(costs[i-1][:j] + costs[i-1][j+1:])
+                costs[i][j]=min(costs[i-1][0:j]+costs[i-1][j+1:])+costs[i][j]
         return min(costs[-1])
-                
