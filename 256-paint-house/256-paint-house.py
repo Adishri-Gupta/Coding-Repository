1ef3b550b70d2f76fa51c3d4f7f80069f66fd065
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-10 00:44:20 -0700

    Time: 70 ms (85.19%), Space: 13.9 MB (88.75%) - LeetHub

--- 256-paint-house/256-paint-house.py
+++ 256-paint-house/256-paint-house.py
@@ -5,6 +5,14 @@ class Solution:
         red=costs[0][0]
         blue=costs[0][1]
         green=costs[0][2]
+        prered=red
+        preblue=blue
+        pregreen=green
         for i in range(1, n):
-            red,blue,green=min(blue,green)+costs[i][0],min(red,green)+costs[i][1],min(red,blue)+costs[i][2]
+            red=min(preblue,pregreen)+costs[i][0]
+            blue=min(prered,pregreen)+costs[i][1]
+            green=min(prered,preblue)+costs[i][2]
+            preblue=blue
+            prered=red
+            pregreen=green
         return min(red,blue,green)
