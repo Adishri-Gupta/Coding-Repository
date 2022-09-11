adbdd69c8ac2bcb16b787fa48feafcdd397f240d
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-11 14:02:05 -0700

    Time: 115 ms (42.05%), Space: 14.1 MB (79.20%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        self.res=[]
+        counter=Counter(nums)
+        
+        self.helper([],counter,nums)
+        return self.res
+    def helper(self,currComb,counter,nums):
+        n=len(nums)
+        
+    
+        if len(currComb)==n:
+            self.res.append(list(currComb))
+        for i in counter:
+            if counter[i]>0:
+                counter[i]-=1
+                self.helper(currComb+[i],counter,nums)
+                counter[i]+=1  
+            
