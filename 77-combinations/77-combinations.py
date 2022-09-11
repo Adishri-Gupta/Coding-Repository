82612b59f38a70b8ad84d81a241fcdd8a5c58fcd
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-11 00:11:24 -0700

    Time: 1018 ms (10.76%), Space: 15.8 MB (85.78%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,13 +1,11 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         self.res=[]
-        self.helper(1,[],k,n)
+        self.helper(k,[],1,n)
         return self.res
-    def helper(self,i,path,k,n):
-        
-        if len(path)==k:
-            self.res.append(list(path))
-            return
-        for i in range (i,n+1):
-            
-            self.helper(i+1,path+[i],k,n)
+    def helper(self,k,currComb,start,n):
+        if len(currComb)==k:
+            self.res.append(list(currComb))
+        for i in range(start,n+1):
+            self.helper(k,currComb+[i],i+1,n)
+  
