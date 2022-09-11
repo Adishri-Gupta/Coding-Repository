502a526151732e42a1864a0d52d70a1640d06a08
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-11 06:00:22 -0700

    Time: 99 ms (5.38%), Space: 14 MB (84.32%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,19 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.res=[]
+        self.helper(nums,[],0,set())
+        return list(self.res)
         
-        self.helper(0,nums,[],set())
-        return self.res
-    def helper(self,start,nums,path, visited):
-        if len(list(path))==len(nums):
-            self.res.append(list(path))
+    def helper(self,nums,currComb,start,visited):
+        n=len(nums)
+        if len(currComb)==n:
+            self.res.append(list(currComb))
             return
         for i in range(len(nums)):
             if nums[i] in visited:
                 continue
             visited.add(nums[i])
-            self.helper(start+1,nums,path+[nums[i]], visited)
+            self.helper(nums,currComb+[nums[i]],i+1,visited)
             visited.remove(nums[i])
-        
-            
-        
