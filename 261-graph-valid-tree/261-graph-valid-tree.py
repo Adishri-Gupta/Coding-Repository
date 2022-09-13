86129447e0d143168f754d2aa06df48bfb137f5d
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-13 03:59:04 -0700

    Time: 223 ms (8.68%), Space: 15.5 MB (54.63%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -11,17 +11,18 @@ class Solution:
             listEdges[i].append(j)
             listEdges[j].append(i)
         return self.isConnected(listEdges)
+    
     def isConnected(self,listEdges):
         visited=set()
         queue=deque([0])
-        visited.add(0)
         while queue:
             ele=queue.popleft()
+            if ele in visited:
+                continue
+            visited.add(ele)
             for nei in listEdges[ele]:
-                if nei in visited:
-                    continue
-                visited.add(nei)
                 queue.append(nei)
+                
         return len(visited)== self.n
     
        
