634f9d625c4d857da0caf2e5809a797660d38563
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-13 03:53:05 -0700

    Time: 188 ms (22.33%), Space: 15.4 MB (74.00%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,14 +1,17 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         listEdges=defaultdict(list)
-        if len(edges)!=n - 1: 
+        self.n=n
+        
+        if len(edges)!=n-1: 
             return False
         
         for edge in edges:
             [i,j]=edge
             listEdges[i].append(j)
             listEdges[j].append(i)
-        
+        return self.isConnected(listEdges)
+    def isConnected(self,listEdges):
         visited=set()
         queue=deque([0])
         visited.add(0)
@@ -19,7 +22,7 @@ class Solution:
                     continue
                 visited.add(nei)
                 queue.append(nei)
-        return len(visited)==n
+        return len(visited)== self.n
     
        
                 
