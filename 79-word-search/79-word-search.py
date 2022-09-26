0ae741d4fe046bda657ec1f37c5f36058d09352c
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-26 14:03:28 -0700

    Time: 5466 ms (76.35%), Space: 13.9 MB (50.77%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,25 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        visited=set()
-        def helper(i,j,pos):
-            if pos==len(word):
-                return True
-            if (i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!=word[pos] or (i,j) in visited):
-                return False
-            visited.add((i,j))
-            res=(helper(i+1,j,pos+1) or 
-                 helper(i,j+1,pos+1) or 
-                 helper(i-1,j,pos+1) or 
-                 helper(i,j-1,pos+1))
-            visited.remove((i,j))
-            return res
-            
-                
-            
-            
-        for i in range(len(board)):
-                for j in range(len(board[0])):
-                    if helper(i,j,0): return True
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if self.search(board, r, c, word, 0):
+                    return True
         return False
+    
+    def search(self, board, row, col, word, i):
+    
+        if i == len(word):
+            return True
+        if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]):
+            return False
+        if board[row][col] != word[i]:
+            return False
+        valS=board[row][col]
+        board[row][col]='#'
+        res = self.search(board,row+1,col,word,i+1)  or self.search(board,row,col+1,word,i+1) or self.search(board,row-1,col,word,i+1) or self.search(board,row,col-1,word,i+1)
+        board[row][col]=valS
+        return res
         
-                
+    
+        
+        
