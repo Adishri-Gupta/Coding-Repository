74d675e152369d52bad5ba9cea6989950eb9c3d9
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-25 17:05:52 -0700

    Time: 263 ms (11.94%), Space: 15.4 MB (89.66%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = {i:[] for i in range(numCourses)}
-        
-        for course,pre in prerequisites:
-            preMap[course].append(pre)
-        
-        visited = set()
-        def dfs(crs):
-          
-            if crs in visited:
-                return False
-            if preMap[crs] == []:
-                return True
+        mapR=defaultdict(list)
+        inDeg=defaultdict(int)
+        ans=[]
+        for pre in prerequisites:
+            x=pre[0]
+            y=pre[1]
+       
+            mapR[y].append(x)
             
-            visited.add(crs)
-            for prereq in preMap[crs]:
-                if not dfs(prereq): return False
-            visited.remove(crs)
-            preMap[crs]=[]
-            return True
+            inDeg[x]+=1
+            
+    
+            
+        q=deque()
+        for i in range(numCourses):
+            if inDeg[i]==0:
+                q.append(i)
+            
+        while q:
+            curr=q.popleft()
+            ans.append(curr)
+            for nei in mapR[curr]:
+                inDeg[nei]-=1
+                if inDeg[nei]==0:
+                    q.append(nei)
+        return len(ans)==numCourses 
         
-        
-        for course in range(numCourses):
-            if not dfs(course): return False
-        return True
