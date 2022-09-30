562fbeb7b4408f6453b3a678248aa11a7cf88215
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-29 17:20:59 -0700

    Time: 102 ms (10.76%), Space: 16.4 MB (80.54%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -7,29 +7,20 @@ import math
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.minMax(root)[0]
-    
-    def minMax(self,node):
-        if not node.left and not node.right:
-            return [True, node.val, node.val]
         
-        is_left_valid, is_right_valid = True, True
+        return self.validate(root,-math.inf,math.inf)
         
-        left_max, right_max = -math.inf, -math.inf
         
-        left_min, right_min = math.inf, math.inf
+    def validate(self,node,low,high):
+        if not node:
+            return True
         
-        if node.left:
-            [is_left_valid, left_min, left_max]=self.minMax(node.left)
-        if node.right:
-            [is_right_valid, right_min, right_max]=self.minMax(node.right)
-
-        if not is_left_valid or not is_right_valid:
-            return [False, math.inf, -math.inf]
+        if node.val<=low or node.val>=high:
+            return False
         
-        is_valid = left_max<node.val<right_min
+        return self.validate(node.right,node.val,high) and self.validate(node.left,low,node.val)
         
-        min_val = min(left_min, right_min, node.val)
-        max_val = max(left_max,right_max, node.val)
         
-        return [is_valid, min_val, max_val]
+        
+            
+        
