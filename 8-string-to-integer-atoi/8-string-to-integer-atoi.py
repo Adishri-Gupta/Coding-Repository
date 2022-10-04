585109f7dac30fdcde3e25d29758f0fd066b6d36
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-10-04 13:27:25 -0700

    Time: 68 ms (35.51%), Space: 13.9 MB (28.33%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -9,10 +9,10 @@ class Solution:
         
         if s[0]=='-' or s[0]=='+':
             temp=s[0]
-            i=1
+            s=s[1:]
         minInt=-2**31
         maxInt=2**31-1
-        for i in range(i,len(s)):
+        for i in range(len(s)):
             if s[i].isdigit():
                 temp+=s[i]
             else:
