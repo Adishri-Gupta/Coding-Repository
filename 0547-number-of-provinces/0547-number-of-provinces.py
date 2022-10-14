60a36e3008a48968a2f4f1e2e7990a81d888843e
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-10-14 13:44:50 -0700

    Time: 454 ms (35.78%), Space: 14.4 MB (55.81%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,20 +1,16 @@
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
         n=len(grid)   
+        visited=set()
+        res=0
         def dfs(i,visited,grid):
             for j in range(n):
                 if grid[i][j] and j not in visited:
                     visited.add(j)
                     dfs(j,visited,grid)
-            
-            
-            
-        
-        visited=set()
-        res=0
+                    
         for i in range(n):
             if i not in visited:
-                
                 visited.add(i)
                 dfs(i,visited,grid)
                 res+=1
