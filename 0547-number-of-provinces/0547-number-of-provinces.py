afd7049e288bc312c431112030e5729d1ec3bb65
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-10-14 14:00:47 -0700

    Time: 695 ms (5.71%), Space: 14.2 MB (78.89%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -3,16 +3,19 @@ class Solution:
         n=len(grid)   
         visited=set()
         res=0
-        def dfs(i,visited,grid):
-            for j in range(n):
-                if grid[i][j] and j not in visited:
-                    visited.add(j)
-                    dfs(j,visited,grid)
-                    
+        q=deque()
+            
         for i in range(n):
             if i not in visited:
                 visited.add(i)
-                dfs(i,visited,grid)
+                q.append(i)
+                while q:
+                    s=q.popleft()
+                
+                    for j in range(n):
+                        if grid[s][j] and j not in visited:
+                            q.append(j)
+                    visited.add(s)
                 res+=1
         return res
             
