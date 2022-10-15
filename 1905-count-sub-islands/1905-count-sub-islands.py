5c35338ba62ebcdfae772bc9f8d7ed55960a7a70
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-10-14 22:44:11 -0700

    Time: 3568 ms (86.50%), Space: 51.8 MB (81.03%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,29 +1,52 @@
 class Solution:
+    
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         m=len(grid2)
         n=len(grid2[0])
-        cnt=0
-        def dfs(i,j):
-          
-            if i<0 or j<0 or i>=len(grid2) or j>=len(grid2[0]) or grid2[i][j]!=1:
-                return
-            grid2[i][j]=-1
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-        for i in range(m):
-            for j in range(n):
-                if grid2[i][j]==1 and grid1[i][j]==0:
-                    dfs(i,j)
-            
-        for i in range(m):
-            for j in range(n):
-                if grid2[i][j]==1:
-                    cnt+=1
-                    dfs(i,j)
-                    
-                    
-        return cnt
-                    
         
+        
+        def findIslands(grid):
+            islands=[]
+            visited=set()
+            for i in range(m):
+                for j in range(n):
+                    if grid[i][j]==1 and (i,j) not in visited:
+                        islands.append(bfs(i,j,grid,visited))
+            return islands        
+                    
+        def bfs(i,j,grid,visited):
+            q=deque()
+            q.append((i,j))
+            island=[]
+            directions=[[-1,0],[1,0],[0,-1],[0,1]]
+            while q:
+                x,y=q.popleft()
+                
+                if (x,y) in visited: continue
+                
+                visited.add((x,y))
+                
+                island.append((x,y))
+                
+                for d in directions:
+                    newx=x+d[0]
+                    newy=y+d[1]
+                    if newx<0 or newy<0 or newx>=m or newy>=n or grid[newx][newy]!=1:
+                        continue
+                    q.append((newx,newy))
+            
+            return island
+        
+        
+        cnt = 0
+        islands2=findIslands(grid2)
+        
+        for i2 in islands2:
+            flag = True
+            for (x,y) in i2:
+                if grid1[x][y] == 0:
+                    flag = False
+                    break
+            cnt += (1 if flag else 0)
+        
+        return cnt
